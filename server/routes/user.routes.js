855bbecd4550a7b5d1261a14ca16776df1f89626
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2024-09-09 12:18:08 +0530

    "Added chalk dependency and new routes to user.routes.js"

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,7 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    return console.log("Welcome to MCA Alumni Network");
+    res.send("Welcome to MCA Alumni Network");
 });
+
+router.get("/about", (req, res)=>{
+    res.send("About MCA Alumni Network");
+});
+
+router.get("/contact", (req, res)=>{
+    res.send("Contact MCA Alumni Network");
+});
+
+router.get("/services", (req, res)=>{
+    res.send("Services of MCA Alumni Network");
+});
+
+router.get("/login", (req, res)=>{
+    res.send("Login to MCA Alumni Network");
+})
+
+router.get("/register", (req, res)=>{
+    res.send("Register to MCA Alumni Network");
+})
+
 
 module.exports = router;
