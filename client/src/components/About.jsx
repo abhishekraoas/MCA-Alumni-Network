ead870c099f57454215a0cb54109a5650445ceac
mdfaizaanalam <faizanalam327@gmail.com>
2024-10-10 19:38:49 +0530

    Update

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -37,7 +37,7 @@ const AboutUs = () => {
       <Box
         sx={{
           height: "90vh",
-          backgroundImage: `url('https://plus.unsplash.com/premium_photo-1673795751644-e42b58452dc0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+          backgroundImage: `url('https://plus.unsplash.com/premium_photo-1672940671025-113a634f83d1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxhY2slMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           display: "flex",
