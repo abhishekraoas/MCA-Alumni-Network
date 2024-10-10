e12b9e97e52f6e3d0d6cf68c5920411d2e1d5902
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-10-10 19:43:52 +0530

    changed issue

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -16,26 +16,26 @@ const About = () => {
               <h1 className='card-title'>
                 MCA<span className='text-orange-600'>ALUMNI</span>  NETWORK
               </h1>
-              <p className='font-semibold text-lg'>
+              <p className='font-semibold text-base'>
                 Our alumni network is a vibrant and diverse community of professionals who have graduated from our MCA program. They are leaders in their fields, making significant contributions to the industry and society. We are proud of their achievements and the positive impact they have on the world.
               </p>
               <div className='card-actions justify-center'>
                 <Link
                   to={`/register`}
-                  className='btn btn-success'>
+                  className='btn btn-success w-28'>
                   Register
                 </Link>
                 <Link
                   to={`/login`}
-                  className='btn btn-primary'>
+                  className='btn btn-primary w-28'>
                   Login
                 </Link>
               </div>
             </div>
           </div>
           <img
-            className='m-5 w-60 lg:w-96 lg:transition-transform lg:duration-0.2 lg:ease-in-out lg:transform lg:hover:scale-105'
-            src={about} />
+            className='m-5 w-60 rounded-md  border-4 border-white lg:w-96 lg:transition-transform lg:duration-0.2 lg:ease-in-out lg:transform lg:hover:scale-105'
+            src={'https://gssoc.girlscript.tech/GirlScript_Group_Photo.svg'} />
         </div>
         <div className='flex flex-col gap-10  fixed top-44 right-5'>
           <a href='https://www.facebook.com/' target='_blank'>
