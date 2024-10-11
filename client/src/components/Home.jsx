f9f214163e2261b0bbf86def37f1ccab4b8d12b1
mdfaizaanalam <faizanalam327@gmail.com>
2024-10-11 09:44:32 +0530

    Update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
       <Box
         sx={{
           height: "95vh",
-          backgroundImage: `url('https://plus.unsplash.com/premium_photo-1701590725824-3d0482721544?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+          backgroundImage: `url('https://plus.unsplash.com/premium_photo-1672940671025-113a634f83d1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxhY2slMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           display: "flex",
