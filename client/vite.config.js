fd73c1df65d7983581a4694acc8dd0579c89a032
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2024-10-25 18:59:15 +0530

    "Updated vite.config.js: added new config block, modified existing config, and removed newline at end of file"

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,19 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
+
+  
+
   theme: {
     extend: {},
   },
   plugins: [],
+}
+
+export const vitaConfig = {
+  eslint: {
+    ignoreDuringBuild: true,
+  },
 }
